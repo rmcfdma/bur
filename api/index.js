c523613815f1d44ec9e59565ceb6653a623f4d3f
Rodrigo Mello da Costa <clarinetabest@gmail.com>
2025-03-28 11:32:55 -0300

    Adicionando o CORS e o BodyParser

--- api/index.js
+++ api/index.js
@@ -1,13 +1,13 @@
 import express from 'express';
-// import bodyParser from "body-parser"
-// import cors from "cors"
+import bodyParser from "body-parser"
+import cors from "cors"
 // import { PrismaClient } from '@prisma/client'
 // import { usuariosRoutes } from './routes/usuariosRoutes.js'
 
 // const prisma = new PrismaClient()
 const app = express();
-// app.use(bodyParser.json());
-// app.use(cors());
+app.use(bodyParser.json());
+app.use(cors());
 app.get('/api', (req, res) => res.send('Express on Vercel'));
 //app.use('/api/usuarios', usuariosRoutes);
 
