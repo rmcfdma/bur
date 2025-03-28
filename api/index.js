2400b4e167f9ed56d8892849673f8c6de5e9c715
Rodrigo Mello da Costa <clarinetabest@gmail.com>
2025-03-28 10:25:26 -0300

    Mudando rota / para testa no index.js

--- api/index.js
+++ api/index.js
@@ -2,7 +2,7 @@ import express from 'express';
 
 const app = express();
 
-app.get('/api/usuarios/count', (req, res) => res.send('Express on Vercel'));
+app.get('/', (req, res) => res.send('Express on Vercel'));
 
 app.listen(3000, () => console.log('Server ready on port 3000.'));
 
