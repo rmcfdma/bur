a3ea9df06f0665e921d7c1d7697a96e17ee8720c
Rodrigo Mello da Costa <clarinetabest@gmail.com>
2025-03-28 11:08:43 -0300

    Acertando os caminhos de importação do router 2

--- api/index.js
+++ api/index.js
@@ -9,7 +9,7 @@ const app = express();
 app.use(bodyParser.json());
 app.use(cors());
 app.get('/api', (req, res) => res.send('Express on Vercel'));
-app.use('/api/usuarios', usuariosRoutes);
+//app.use('/api/usuarios', usuariosRoutes);
 
 //app.listen(3000, () => console.log('Server ready on port 3000.'));
 
