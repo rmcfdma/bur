918c71f2baddf823f0df37cb4759e7d0fd42c554
Rodrigo Mello da Costa <clarinetabest@gmail.com>
2025-03-29 21:00:49 -0300

    Testando o express use

--- api/index.js
+++ api/index.js
@@ -1,16 +1,17 @@
 import express from 'express';
 import bodyParser from "body-parser"
 import cors from "cors"
-import { PrismaClient } from '@prisma/client'
+//import { PrismaClient } from '@prisma/client'
 import usuariosRoutes from '../routes/usuariosRoutes.js'
 
-const prisma = new PrismaClient()
+//const prisma = new PrismaClient()
 const app = express();
 app.use(bodyParser.json());
 app.use(cors());
-app.use('/api', (req, res) => res.send('Express on Vercel'));
-app.use('/api/usuarios', usuariosRoutes);
 
-app.listen(3000, () => console.log('Server ready on port 3000.'));
+app.use('/api', (req, res) => res.send('Express on Vercel'));
+app.use('/api/usuarios', (req, res) => res.send('Entrei no usuarios'));
+
+//app.listen(3000, () => console.log('Server ready on port 3000.'));
 
 export default app;
