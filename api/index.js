d2ff0511e04fcaf96a8851712f54169c4b227af2
Rodrigo Mello da Costa <clarinetabest@gmail.com>
2025-03-29 21:05:31 -0300

    Testando o express use 1

--- api/index.js
+++ api/index.js
@@ -9,7 +9,7 @@ const app = express();
 app.use(bodyParser.json());
 app.use(cors());
 
-app.use('/api', (req, res) => res.send('Express on Vercel'));
+app.use('/api', (req, res) => res.send('Express on Vercel total'));
 app.use('/api/usuarios', (req, res) => res.send('Entrei no usuarios'));
 
 //app.listen(3000, () => console.log('Server ready on port 3000.'));
