055be9b69bfff51075a4b35c60575af2c46299c7
Rodrigo Mello da Costa <clarinetabest@gmail.com>
2025-03-29 22:41:45 -0300

    Alterando o vercel.json 11

--- api/index.js
+++ api/index.js
@@ -1,18 +1,12 @@
 import express from 'express';
 import bodyParser from "body-parser"
 import cors from "cors"
-//import { PrismaClient } from '@prisma/client'
 import usuariosRoutes from '../routes/usuariosRoutes.js'
 
-//const prisma = new PrismaClient()
 const app = express();
 app.use(bodyParser.json());
 app.use(cors());
 
 app.use('/api/usuarios', usuariosRoutes);
-//app.get('/api', (req, res) => res.send('Entrei em /api'));
-
-
-//app.listen(3000, () => console.log('Server ready on port 3000.'));
 
 export default app;
